3e6a063fd59e21c0f680309e4695685d405a4f47
huangrungen_1 <1165921036@qq.com>
2023-12-23 10:43:46 +0800

    dt

--- diantao.py
+++ diantao.py
@@ -11,6 +11,7 @@ from utils import hd
 d = u2.connect("192.168.168.190:5566")  #
 width, height = d.window_size()
 print(d.info)
+d.app_stop("com.taobao.live")
 d.app_start("com.taobao.live")
 d.sleep(3)
 
@@ -32,7 +33,7 @@ while(len(d(text="去完成"))>0):
     d(text="去完成").click()
     d.sleep(1)
     runtime = time.time()
-    if(d(text="奖励将于").exists):
+    if(d(text="奖励将于").exists or d(text="跳过").exists()):
         d.sleep(50)
         d(resourceId="android:id/button2").click_exists()
     else:
@@ -41,8 +42,13 @@ while(len(d(text="去完成"))>0):
             d(resourceId="android:id/button2").click_exists()
     d.press("back")
     d(resourceId="com.byted.pangle.m:id/tt_reward_full_count_down_after").click_exists()
+    d(resourceId="com.taobao.live:id/negative").click_exists()
     if(d(text="已领取奖励").exists):
         d(text="跳过").click()
+    d(text="跳过").click_exists()
+    d.sleep(0.5)
+    d(resourceId="com.byted.pangle.m:id/tt_negtive").click_exists()
+    d(text="坚持退出").click_exists()
     d.sleep(1)
     d(text="继续做任务").click_exists()
 
