a154b250edc81a2784e09eab206b258bde22359b
hrg_2 <1165921036@qq.com>
2024-01-15 21:31:10 +0800

    update

--- swipe.py
+++ swipe.py
@@ -1,3 +1,5 @@
+import os
+
 import uiautomator2 as u2
 import random
 import time
@@ -5,28 +7,30 @@ from utils import hd,TimePrint
 
 
 # d = u2.connect_usb("c635d924")  # id 为 adb devices 命令中得到的设备 id
-
+#
 # 无线连接
 
-d = u2.connect("192.168.1.103:5566")  #
+d = u2.connect("192.168.168.190:5566")  #
+# d = u2.connect("192.168.1.103:5566")  #
 print(d.info)
 width, height = d.window_size()
 startTm = time.time()
+os.system("python -m weditor")
 # print(d.xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]').click())
 # for elem in d.xpath('//android.widget.TextView').all():
 #     print(elem)
 #     print("Text:",elem.text)
-while(True):
-    # hd(d,40,60,startTm)
-    width, height = d.window_size()
-    sx = random.randrange(650, 850) / 1000.0
-    ex = random.randrange(200, 300) / 1000.0
-    sy = random.randrange(450, 650) / 1000.0
-    ey = random.randrange(400, 650) / 1000.0
-    dur = random.randrange(67, 146) / 1000.0
-    d.swipe(sx * width, sy * height, ex * width, ey * height, duration=dur)
-    second = random.randrange(4, 7)
-    d.sleep(second)
-    
-    print("随机等待" + str(second) + "s")
-    TimePrint(startTm)
+# while(True):
+    # hd(d,6,10,startTm)
+    # width, height = d.window_size()
+    # sx = random.randrange(650, 850) / 1000.0
+    # ex = random.randrange(200, 300) / 1000.0
+    # sy = random.randrange(450, 650) / 1000.0
+    # ey = random.randrange(400, 650) / 1000.0
+    # dur = random.randrange(67, 146) / 1000.0
+    # d.swipe(sx * width, sy * height, ex * width, ey * height, duration=dur)
+    # second = random.randrange(4, 7)
+    # d.sleep(second)
+    #
+    # print("随机等待" + str(second) + "s")
+    # TimePrint(startTm)
--- zfb.py
+++ zfb.py
@@ -3,19 +3,25 @@ import random
 import time
 from utils import hd
 startTm = time.time()
-d = u2.connect("192.168.168.190:5566")
+d = u2.connect()
+# d = u2.connect("192.168.168.190:5566")
+# d = u2.connect("192.168.168.167:5568")
 print(d.info)
+d.app_stop("com.eg.android.AlipayGphone")
 d.app_start("com.eg.android.AlipayGphone")
-d.sleep(2)
-d(resourceId="com.alipay.android.tablauncher:id/tab_description", text="生活").click()
+# d.sleep(2)
+width, height = d.window_size()
+# d(resourceId="com.alipay.android.tablauncher:id/tab_description", text="生活").click()
+# d(resourceId="com.alipay.android.tablauncher:id/tab_description", text="视频").click()
+# d.click(0.5*width,0.90*height)
 
-d(resourceId="com.alipay.android.living.dynamic:id/bgImageView").click_exists()
-d(text="立即签到领").click_exists()
+d(resourceId="com.alipay.android.living.dynamic:id/bgImageView").click_exists(timeout=1)
+d(text="立即签到领").click_exists(timeout=2)
 
-d.xpath('//*[@resource-id="com.alipay.android.living.dynamic:id/tab_layout"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[4]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]').click()
+# d.xpath('//*[@resource-id="com.alipay.android.living.dynamic:id/tab_layout"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[4]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]').click()
 while(True):
     hd(d,5,8,startTm)
     if(d(resourceId="com.alipay.android.living.dynamic:id/redPacketIv").exists() == False):
         d.press("back")
     now = time.time()
-    if(now-startTm>1200):break
+    if(now-startTm>320000):break
