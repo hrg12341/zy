60e74e95f10769329a81bf01df72e7b74f18422a
hrg_2 <1165921036@qq.com>
2024-01-18 08:20:01 +0800

    main

---
+++ DNF/main.py
@@ -0,0 +1,22 @@
+import os
+
+# 1.女枪
+# 2.奶妈
+# 3.旅人
+# 4.缪斯
+# 5.花花
+# 6.风法
+# 7.阿修罗
+# 8.召唤
+
+type = 1
+pl = 188
+
+def shenyuan():
+    os.system("python shenyuan.py " + str(pl) + " " + str(type))
+
+def fengbao():
+    os.system("python fengbao.py " + str(pl) + " " + str(type))
+
+shenyuan()
+# fengbao()
