fa1200ea7fb421045d1adf279e0837593a35b597
hrg_2 <1165921036@qq.com>
2024-03-13 12:24:10 +0800

    update

--- utils.py
+++ utils.py
@@ -25,3 +25,18 @@ def hd(d,start,end,startTm):
 
     print("随机等待"+str(second)+"s")
     TimePrint(startTm)
+
+def hd_zfb(d,start,end,startTm):
+    width, height = d.window_size()
+    second = random.randrange(start,end)
+    d.sleep(second)
+    print("随机等待"+str(second)+"s")
+    TimePrint(startTm)
+    sx = random.randrange(450,650)/1000.0
+    ex = random.randrange(500,650)/1000.0
+    sy = random.randrange(750,850)/1000.0
+    ey = random.randrange(300,550)/1000.0
+    dur = random.randrange(187,256)/1000.0
+    d.swipe(sx*width,sy*height,ex*width,ey*height,duration=dur)
+
+
--- zfb.py
+++ zfb.py
@@ -1,14 +1,15 @@
 import uiautomator2 as u2
 import random
 import time
-from utils import hd
+from utils import hd,hd_zfb
 startTm = time.time()
-d = u2.connect()
+# d = u2.connect()
 # d = u2.connect("192.168.168.190:5566")
 # d = u2.connect("192.168.168.167:5568")
+d = u2.connect("192.168.1.101:5566")  #
 print(d.info)
-d.app_stop("com.eg.android.AlipayGphone")
-d.app_start("com.eg.android.AlipayGphone")
+# d.app_stop("com.eg.android.AlipayGphone")
+# d.app_start("com.eg.android.AlipayGphone")
 # d.sleep(2)
 width, height = d.window_size()
 # d(resourceId="com.alipay.android.tablauncher:id/tab_description", text="生活").click()
@@ -17,11 +18,13 @@ width, height = d.window_size()
 
 d(resourceId="com.alipay.android.living.dynamic:id/bgImageView").click_exists(timeout=1)
 d(text="立即签到领").click_exists(timeout=2)
-
+hd(d,3,7,startTm)
 # d.xpath('//*[@resource-id="com.alipay.android.living.dynamic:id/tab_layout"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[4]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]').click()
 while(True):
-    hd(d,5,8,startTm)
-    if(d(resourceId="com.alipay.android.living.dynamic:id/redPacketIv").exists() == False):
-        d.press("back")
+    hd_zfb(d,8,18,startTm)
+    time.sleep(3)
+    if(d(resourceId="com.alipay.android.living.dynamic:id/image").exists() == False):
+        hd(d,1,2,startTm)
+
     now = time.time()
     if(now-startTm>320000):break
